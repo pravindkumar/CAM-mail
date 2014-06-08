ef51de5898cd0b765d3b0022c61589a52efd13a4
pravindkumar <pravind.india@gmail.com>
2014-06-09 02:04:33 +0530

    Update INF.java

--- CAM@mail/src/com/pravind/pcameraproject/INF.java
+++ CAM@mail/src/com/pravind/pcameraproject/INF.java
@@ -22,132 +22,132 @@ import android.widget.Toast;
 
 import com.pravind.cameraproject.R;
 
-public class INF extends CameraProject {
-	Bitmap bitmap;
+public class INF extends CameraProject 
+{
+    Bitmap bitmap;
     String pic,fullPath;
     String[] Paths;
     int era;
     Intent imageReturnedIntent;
     final static int CameraData = 0;
-	ArrayList<Uri> uris;
-	File fileIn;
+    ArrayList<Uri> uris;
+    File fileIn;
 	
     
     private static final String TAG = "MyActivity";
-	protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) 
+    	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.in);
-		
 		Button bt = (Button)findViewById(R.id.bt);
-		
 		preview();
-		
 	}
 		//to show the image	
-	private void preview() {
-		getLastImageId();
-		File imgFile = new  File(fullPath);
-		if(imgFile.exists()){
-
-		    Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
-
-		    ImageView myImage = (ImageView) findViewById(R.id.imageView1);
-		    myImage.setImageBitmap(myBitmap);
+    private void preview() 
+    {
+	getLastImageId();
+	File imgFile = new  File(fullPath);
+	if(imgFile.exists())
+	{
+	    Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+	    ImageView myImage = (ImageView) findViewById(R.id.imageView1);
+	    myImage.setImageBitmap(myBitmap);
 		    //generate toast message
-		    Context context = getApplicationContext();
-			CharSequence text = "image shown clearly or not ,if not than press back";
-			int duration = Toast.LENGTH_LONG;
-
-			Toast toast = Toast.makeText(context, text, duration);
+	    Context context = getApplicationContext();
+	    CharSequence text = "image shown clearly or not ,if not than press back";
+	    int duration = Toast.LENGTH_LONG;
+        	Toast toast = Toast.makeText(context, text, duration);
 			toast.show();
-			
-
-		}
-		
 	}
+     }
 
-	public void Takepath(){
+	public void Takepath()
+	{
 		getLastImageId();
 		uris = new ArrayList<Uri>();
 		Paths = new String[] {fullPath};
-		for(String file : Paths ){
-	    fileIn= new File(file);
-		Uri u=Uri.fromFile(fileIn);
-		uris.add(u);
+		for(String file : Paths )
+		{
+	        	fileIn= new File(file);
+			Uri u=Uri.fromFile(fileIn);
+			uris.add(u);
 			
 		}
-		
 	}
 	//perform sending action
 			
-			public void sedmail(View v){
-				Takepath();
-				final EditText et = (EditText)findViewById(R.id.et1);
-				final EditText et2 = (EditText)findViewById(R.id.et2);
-				
-				Intent i = new Intent(android.content.Intent.ACTION_SEND_MULTIPLE);
-				i.setType("plain/text");
-				i.setType("application/octet-stream");
-				
-				i.putParcelableArrayListExtra(Intent.EXTRA_STREAM, uris);
-				i.putExtra(Intent.ACTION_DEFAULT, "test/");
-				
-				i.putExtra(Intent.EXTRA_EMAIL, new String[]{"pravind.india@gmail.com"});
-				i.putExtra(Intent.EXTRA_SUBJECT, "My Information");
-				i.putExtra(Intent.EXTRA_TEXT, "Name:"+et.getText().toString()+'\n'+"Mobile No."+et2.getText().toString());	
-				startActivity(Intent.createChooser(i, "send email...."));
-			}
+	public void sedmail(View v
+	{
+		Takepath();
+		final EditText et = (EditText)findViewById(R.id.et1);
+		final EditText et2 = (EditText)findViewById(R.id.et2);
+		Intent i = new Intent(android.content.Intent.ACTION_SEND_MULTIPLE);
+		i.setType("plain/text");
+		i.setType("application/octet-stream");
+		i.putParcelableArrayListExtra(Intent.EXTRA_STREAM, uris);
+		i.putExtra(Intent.ACTION_DEFAULT, "test/");
+		i.putExtra(Intent.EXTRA_EMAIL, new String[]{"pravind.india@gmail.com"});//here you can put any email adderess where you want to send informatio
+		i.putExtra(Intent.EXTRA_SUBJECT, "My Information");
+		i.putExtra(Intent.EXTRA_TEXT, "Name:"+et.getText().toString()+'\n'+"Mobile No."+et2.getText().toString());	
+		startActivity(Intent.createChooser(i, "send email...."));
+	}
 	
-	//call camera when back button is pressed
-		public void cam(View v) {
-			finish();
-		}
 
-		
-		@Override
-		public void onBackPressed() {
+	public void cam(View v) 
+	{
+		finish();
+	}
+
+			//call camera when back button is pressed
+	@Override
+	public void onBackPressed() 
+	{
 		finish();
 		Intent i = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
 		startActivityForResult(i, CameraData);
-		}
+	}
 
-		@Override
-		protected void onActivityResult(int requestCode, int resultCode, Intent data){
-			super.onActivityResult(requestCode, resultCode, data);
-			if (resultCode == RESULT_OK) {
-				Bundle extras = data.getExtras();
-				btm = (Bitmap) extras.get("data");
-								
-			}
-			    if(resultCode == RESULT_OK){
-					Intent intent1=new Intent (this,INF.class);
-					startActivity(intent1);
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data)
+	{
+		super.onActivityResult(requestCode, resultCode, data);
+		if (resultCode == RESULT_OK)
+		{
+			Bundle extras = data.getExtras();
+			btm = (Bitmap) extras.get("data");
 		}
-			    else {
-			    	
-			    	Intent i = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-					startActivityForResult(i, CameraData);
-			    }
-			   
-		
+	        if(resultCode == RESULT_OK)
+	        {
+			Intent intent1=new Intent (this,INF.class);
+			startActivity(intent1);
 		}
+	        else 
+	        {
+		    	Intent i = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+			startActivityForResult(i, CameraData);
+	         }
+	}
 			//get the id of the last image taken by camera			
-	private int getLastImageId(){
+	private int getLastImageId()
+	{
 	    final String[] imageColumns = { MediaStore.Images.Media._ID, MediaStore.Images.Media.DATA };
 	    final String imageOrderBy = MediaStore.Images.Media._ID+" DESC";
 	    Cursor imageCursor = managedQuery(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, imageColumns, null, null, imageOrderBy);
-	    if(imageCursor.moveToFirst()){
-	        int id = imageCursor.getInt(imageCursor.getColumnIndex(MediaStore.Images.Media._ID));
-	         fullPath = imageCursor.getString(imageCursor.getColumnIndex(MediaStore.Images.Media.DATA));
+	    if(imageCursor.moveToFirst())
+	    {
+	    	int id = imageCursor.getInt(imageCursor.getColumnIndex(MediaStore.Images.Media._ID));
+		fullPath = imageCursor.getString(imageCursor.getColumnIndex(MediaStore.Images.Media.DATA));
 	        Log.d(TAG, "getLastImageId::id " + id);
 	        Log.d(TAG, "getLastImageId::path " + fullPath);
-	       imageCursor.close();
+	        imageCursor.close();
 	        return id;
-	    }else{
+	    }
+	    else
+	    {
 	        return 0;
 	    }
-	}
+	 }
 	
-	}
+}
 
 
